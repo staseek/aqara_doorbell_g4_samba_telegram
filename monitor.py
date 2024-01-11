5c804e4a8f2d588af09cf31da1aea2a20196356c
Stanislav <stasyatr@gmail.com>
2024-01-11 23:00:40 +0300

    (Added) moving necessary files

--- monitor.py
+++ monitor.py
@@ -4,21 +4,24 @@ This script monitors a directory and sends new files to the telegram chat.
 import os
 import time
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+from watchdog.events import LoggingEventHandler, FileClosedEvent
 from dotenv import load_dotenv
 from aiogram import Bot
+
 
 class EndWritingHandler(LoggingEventHandler):
     """
     Custom handler. It works only on Linux-based OSes
     """
-    def __init__(self, tg_bot_token, tg_chat_id):
+    def __init__(self, tg_bot_token: str, tg_chat_id: str):
+        super().__init__()
         self.tg_bot_token = tg_bot_token
         self.tg_chat_id = tg_chat_id
         self.bot = Bot(self.tg_bot_token)
 
-    def on_closed(self, event):
-        print('closed event!')
+    def on_closed(self, event: FileClosedEvent):
+        if not event.is_directory:
+            self.bot.send_video(self.tg_chat_id, event.src_path)
 
 
 def main():
@@ -30,7 +33,7 @@ def main():
     path = os.environ['MONITORING_FOLDER_VIDEO']
     telegram_chat_id = os.environ['TG_CHAT_ID']
     telegram_token = os.environ['TG_BOT_TOKEN']
-    event_handler = EndWritingHandler()
+    event_handler = EndWritingHandler(telegram_token, telegram_chat_id)
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
