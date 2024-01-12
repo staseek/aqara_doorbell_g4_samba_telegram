1a9cda653f2bdeff2fa47c42864ae8c43b17b1ff
Stanislav <stasyatr@gmail.com>
2024-01-12 18:46:26 +0300

    (Fixed) working with loop and executer

--- monitor.py
+++ monitor.py
@@ -9,20 +9,23 @@ from dotenv import load_dotenv
 from aiogram import Bot
 from aiogram.types import FSInputFile
 import asyncio
-
+from concurrent.futures import ProcessPoolExecutor
 
 class EndWritingHandler(LoggingEventHandler):
     """
     Custom handler. It works only on Linux-based OSes
     """
-    def __init__(self, tg_bot_token: str, tg_chat_id: str):
+    def __init__(self, tg_bot_token: str, tg_chat_id: str, loop):
         super().__init__()
         self.tg_bot_token = tg_bot_token
         self.tg_chat_id = tg_chat_id
         self.bot = Bot(self.tg_bot_token)
+        self.loop = loop
 
     async def bbb(self, event):
         try:
+            await self.bot.send_message(chat_id=self.tg_chat_id,
+                                        text=event.src_path)
             await self.bot.send_video(chat_id=self.tg_chat_id,
                                 video=FSInputFile(event.src_path))
         except Exception as e:
@@ -31,11 +34,7 @@ class EndWritingHandler(LoggingEventHandler):
     def on_closed(self, event: FileClosedEvent):
         print(event.is_directory, event.src_path)
         if not event.is_directory:
-            # print(event.src_path)
-            asyncio.set_event_loop(asyncio.new_event_loop())
-            asyncio.get_event_loop().run_until_complete(self.bot.send_message(self.tg_chat_id, event.src_path))
-            asyncio.set_event_loop(asyncio.new_event_loop())
-            asyncio.get_event_loop().run_until_complete(self.bbb(event))
+            self.loop.run_until_complete(self.bbb(event))
 
 
 def main():
@@ -47,7 +46,10 @@ def main():
     path = os.environ['MONITORING_FOLDER_VIDEO']
     telegram_chat_id = os.environ['TG_CHAT_ID']
     telegram_token = os.environ['TG_BOT_TOKEN']
-    event_handler = EndWritingHandler(telegram_token, telegram_chat_id)
+    loop = asyncio.get_event_loop()
+    p = ProcessPoolExecutor(2)  # Create a ProcessPool with 2 processes
+
+    event_handler = EndWritingHandler(telegram_token, telegram_chat_id, loop)
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
